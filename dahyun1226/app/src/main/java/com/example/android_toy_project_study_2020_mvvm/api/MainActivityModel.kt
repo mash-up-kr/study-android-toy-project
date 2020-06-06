de10da9859860914a733507d80a06fd297bcac50
Dahyun Kang <dahyun1238@gmail.com>
2020-06-06 14:52:48 +0900

    MainActivityModel 변수명 수정

--- dahyun1226/app/src/main/java/com/example/android_toy_project_study_2020_mvvm/api/MainActivityModel.kt
+++ dahyun1226/app/src/main/java/com/example/android_toy_project_study_2020_mvvm/api/MainActivityModel.kt
@@ -11,46 +11,53 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class MainActivityModel (MainActivity: Activity){
-    val mainActivity: Activity = MainActivity
-    fun githubSearch(keyword: String){
-        RetrofitService.getService().requestGithubResponse(keyword = keyword).enqueue(object: Callback<GithubResponseData> {
-            override fun onFailure(call: Call<GithubResponseData>, t: Throwable) {
-                mainActivity.loading.visibility = INVISIBLE
-                mainActivity.errorTextViewRepository.text = "에러발생 : " + t.toString()
-                mainActivity.errorTextViewRepository.visibility = VISIBLE
-                mainActivity.recycler.visibility = INVISIBLE
-            }
-            override fun onResponse(call: Call<GithubResponseData>, response: Response<GithubResponseData>) {
-                if (response.isSuccessful) {
+class MainActivityModel(_mainActivity: Activity) {
+    val mainActivity = _mainActivity
+    fun githubSearch(keyword: String) {
+        RetrofitService.getService().requestGithubResponse(keyword = keyword)
+            .enqueue(object : Callback<GithubResponseData> {
+                override fun onFailure(call: Call<GithubResponseData>, t: Throwable) {
                     mainActivity.loading.visibility = INVISIBLE
-                    mainActivity.errorTextViewRepository.visibility = INVISIBLE
-                    val githubResponseData = response.body()
-                    mainActivity.loading.visibility = INVISIBLE
-                    val list=ArrayList<GitItem>()
-                    if (githubResponseData != null) {
-                        if (githubResponseData.items.isEmpty()){
-                            mainActivity.errorTextViewRepository.text = "결과 없음!!!"
-                            mainActivity.errorTextViewRepository.visibility = VISIBLE
-                            mainActivity.recycler.visibility = INVISIBLE
-                        } else {
-                            for (i in githubResponseData.items.indices) {
-                                list.add(
-                                    GitItem(
-                                        githubResponseData.items[i].owner.avatar_url,
-                                        githubResponseData.items[i].full_name,
-                                        githubResponseData.items[i].language
+                    mainActivity.errorTextViewRepository.text = "에러발생 : " + t.toString()
+                    mainActivity.errorTextViewRepository.visibility = VISIBLE
+                    mainActivity.recycler.visibility = INVISIBLE
+                }
+
+                override fun onResponse(
+                    call: Call<GithubResponseData>,
+                    response: Response<GithubResponseData>
+                ) {
+                    if (response.isSuccessful) {
+                        mainActivity.loading.visibility = INVISIBLE
+                        mainActivity.errorTextViewRepository.visibility = INVISIBLE
+                        val githubResponseData = response.body()
+                        mainActivity.loading.visibility = INVISIBLE
+                        val list = ArrayList<GitItem>()
+                        if (githubResponseData != null) {
+                            if (githubResponseData.items.isEmpty()) {
+                                mainActivity.errorTextViewRepository.text = "결과 없음!!!"
+                                mainActivity.errorTextViewRepository.visibility = VISIBLE
+                                mainActivity.recycler.visibility = INVISIBLE
+                            } else {
+                                for (i in githubResponseData.items.indices) {
+                                    list.add(
+                                        GitItem(
+                                            githubResponseData.items[i].owner.avatar_url,
+                                            githubResponseData.items[i].full_name,
+                                            githubResponseData.items[i].language
+                                        )
                                     )
-                                )
+                                }
+                                val adapter = ItemAdapter(list)
+                                mainActivity.recycler.adapter = adapter
+                                mainActivity.recycler.visibility = VISIBLE
                             }
-                            val adapter = ItemAdapter(list)
-                            mainActivity.recycler.adapter = adapter
-                            mainActivity.recycler.visibility = VISIBLE
                         }
+                    } else {
+
                     }
                 }
-            }
 
-        })
+            })
     }
 }
