6952f59e036e088e89300307672f9c268180e87b
Dahyun Kang <dahyun1238@gmail.com>
2020-06-11 22:01:19 +0900

    add BaseResponse interface

---
+++ dahyun1226/app/src/main/java/com/example/android_toy_project_study_2020_mvvm/model/BaseResponse.kt
@@ -0,0 +1,14 @@
+package com.example.android_toy_project_study_2020_mvvm.model
+
+interface BaseResponse<T> {
+
+    fun onSuccess(data: T)
+
+    fun onFail(description: String)
+
+    fun onError(throwable: Throwable)
+
+    fun onLoading()
+
+    fun onLoaded()
+}
