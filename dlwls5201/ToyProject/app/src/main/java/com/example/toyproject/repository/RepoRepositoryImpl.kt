e0bc85f6f030d6f4048a4993827c8976824f79cd
leejinseong <dlwls5201@naver.com>
2020-06-12 02:06:29 +0900

    리포지터리 Rx로 구현

--- dlwls5201/ToyProject/app/src/main/java/com/example/toyproject/repository/RepoRepositoryImpl.kt
+++ dlwls5201/ToyProject/app/src/main/java/com/example/toyproject/repository/RepoRepositoryImpl.kt
@@ -7,104 +7,80 @@ import com.example.toyproject.data.model.RepoDetailModel
 import com.example.toyproject.data.model.RepoModel
 import com.example.toyproject.data.model.RepoSearchResponse
 import com.example.toyproject.data.model.UserModel
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import io.reactivex.Single
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.functions.BiFunction
+import retrofit2.HttpException
 
 class RepoRepositoryImpl(
     private val repoApi: RepoApi,
     private val userApi: UserApi
 ) : RepoRepository {
 
-    override fun searchRepositories(query: String, callback: BaseResponse<RepoSearchResponse>) {
-        callback.onLoading()
-
-        repoApi.searchRepository(query)
-            .enqueue(object : Callback<RepoSearchResponse> {
-                override fun onResponse(
-                    call: Call<RepoSearchResponse>,
-                    response: Response<RepoSearchResponse>
-                ) {
-                    val body = response.body()
-                    if (response.isSuccessful && null != body) {
-                        callback.onSuccess(body)
-                    } else {
-                        callback.onFail(response.message())
-                    }
-
-                    callback.onLoaded()
+    override fun searchRepositories(
+        query: String,
+        callback: BaseResponse<RepoSearchResponse>
+    ): Disposable {
+        return repoApi.searchRepository(query)
+            //이 이후에 수행되는 코드는 메인 쓰레드에서 실행됩니다.
+            .observeOn(AndroidSchedulers.mainThread())
+            //구독할 때 수행할 작업을 구현합니다.
+            .doOnSubscribe {
+                callback.onLoading()
+            }
+            //스트림이 종료될 때 수행할 작업을 구현합니다.
+            //Single에서 스트림이 종료되는 시점은 성공(Success)과 에러(Error) 입니다.
+            .doOnTerminate {
+                callback.onLoaded()
+            }
+            //옵서버블을 구독합니다.
+            .subscribe({
+                callback.onSuccess(it)
+            }) {
+                // 에러 처리 작업을 구현합니다.
+                if (it is HttpException) {
+                    callback.onFail(it.message())
+                } else {
+                    callback.onError(it)
                 }
-
-                override fun onFailure(call: Call<RepoSearchResponse>, t: Throwable) {
-                    callback.onError(t)
-                    callback.onLoaded()
-                }
-            })
+            }
     }
 
     override fun getDetailRepository(
         user: String, repo: String, callback: BaseResponse<RepoDetailModel>
-    ) {
-        callback.onLoading()
-
-        repoApi.getRepository(user, repo)
-            .enqueue(object : Callback<RepoModel> {
-                override fun onResponse(
-                    call: Call<RepoModel>,
-                    response: Response<RepoModel>
-                ) {
-                    val body = response.body()
-                    if (response.isSuccessful && null != body) {
-                        val repoModel = body
-
-                        userApi.getUser(user)
-                            .enqueue(object : Callback<UserModel> {
-                                override fun onResponse(
-                                    call: Call<UserModel>,
-                                    response: Response<UserModel>
-                                ) {
-                                    val body = response.body()
-                                    if (response.isSuccessful && null != body) {
-                                        val userModel = body
-
-                                        callback.onSuccess(
-                                            RepoDetailModel(
-                                                title = repoModel.fullName,
-                                                repoName = repoModel.name,
-                                                ownerName = userModel.name,
-                                                ownerUrl = userModel.profileImgUrl,
-                                                followers = userModel.followers,
-                                                following = userModel.following,
-                                                description = repoModel.description,
-                                                language = repoModel.language,
-                                                updatedAt = repoModel.updatedAt,
-                                                stars = repoModel.stars
-                                            )
-                                        )
-                                    } else {
-                                        callback.onFail(response.message())
-                                    }
-
-                                    callback.onLoaded()
-                                }
-
-                                override fun onFailure(call: Call<UserModel>, t: Throwable) {
-                                    callback.onError(t)
-                                    callback.onLoaded()
-                                }
-
-                            })
-
-                    } else {
-                        callback.onFail(response.message())
-                        callback.onLoaded()
-                    }
+    ): Disposable {
+        return Single.zip(
+            repoApi.getRepository(user, repo), userApi.getUser(user),
+            BiFunction<RepoModel, UserModel, RepoDetailModel> { repoModel, userModel ->
+                RepoDetailModel(
+                    title = repoModel.fullName,
+                    repoName = repoModel.name,
+                    ownerName = userModel.name,
+                    ownerUrl = userModel.profileImgUrl,
+                    followers = userModel.followers,
+                    following = userModel.following,
+                    description = repoModel.description,
+                    language = repoModel.language,
+                    updatedAt = repoModel.updatedAt,
+                    stars = repoModel.stars
+                )
+            }
+        ).observeOn(AndroidSchedulers.mainThread())
+            .doOnSubscribe {
+                callback.onLoading()
+            }
+            .doOnTerminate {
+                callback.onLoaded()
+            }
+            .subscribe({
+                callback.onSuccess(it)
+            }) {
+                if (it is HttpException) {
+                    callback.onFail(it.message())
+                } else {
+                    callback.onError(it)
                 }
-
-                override fun onFailure(call: Call<RepoModel>, t: Throwable) {
-                    callback.onError(t)
-                    callback.onLoaded()
-                }
-            })
+            }
     }
 }
--- dlwls5201/ToyProject/app/src/main/java/com/example/toyproject/repository/fake/FakeRepoRepositoryImpl.kt
+++ dlwls5201/ToyProject/app/src/main/java/com/example/toyproject/repository/fake/FakeRepoRepositoryImpl.kt
@@ -1,63 +1,80 @@
 package com.example.toyproject.repository.fake
 
-import android.os.Handler
 import com.example.toyproject.data.base.BaseResponse
 import com.example.toyproject.data.model.RepoDetailModel
 import com.example.toyproject.data.model.RepoModel
 import com.example.toyproject.data.model.RepoSearchResponse
 import com.example.toyproject.repository.RepoRepository
+import io.reactivex.Single
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
 import java.util.*
+import java.util.concurrent.TimeUnit
 
 class FakeRepoRepositoryImpl : RepoRepository {
 
-    override fun searchRepositories(query: String, callback: BaseResponse<RepoSearchResponse>) {
-        callback.onLoading()
+    private val DELAY_TIME = 5000L
 
-        Handler().postDelayed({
-            callback.onSuccess(
-                RepoSearchResponse(
-                    totalCount = 10,
-                    items = listOf(
-                        RepoModel(
-                            name = "name",
-                            fullName = "fullName",
-                            owner = RepoModel.OwnerModel(
-                                login = "login",
-                                avatarUrl = ""
-                            ),
-                            description = null,
-                            language = null,
-                            updatedAt = Date(),
-                            stars = 1000
-                        )
+    override fun searchRepositories(
+        query: String, callback: BaseResponse<RepoSearchResponse>
+    ): Disposable {
+        return Single.just(
+            RepoSearchResponse(
+                totalCount = 10,
+                items = listOf(
+                    RepoModel(
+                        name = "name",
+                        fullName = "fullName",
+                        owner = RepoModel.OwnerModel(
+                            login = "login",
+                            avatarUrl = ""
+                        ),
+                        description = null,
+                        language = null,
+                        updatedAt = Date(),
+                        stars = 1000
                     )
                 )
             )
-            callback.onLoaded()
-        }, 1000)
+        ).delay(DELAY_TIME, TimeUnit.MILLISECONDS)
+            .observeOn(AndroidSchedulers.mainThread())
+            .doOnSubscribe {
+                callback.onLoading()
+            }.doOnTerminate {
+                callback.onLoaded()
+            }.subscribe({
+                callback.onSuccess(it)
+            }) {
+                callback.onError(it)
+            }
     }
 
     override fun getDetailRepository(
         user: String, repo: String, callback: BaseResponse<RepoDetailModel>
-    ) {
-        callback.onLoading()
-
-        Handler().postDelayed({
-            callback.onSuccess(
-                RepoDetailModel(
-                    title = "title",
-                    repoName = "repoName",
-                    ownerName = "ownerName",
-                    ownerUrl = "",
-                    followers = 1000,
-                    following = 1000,
-                    description = "description",
-                    language = null,
-                    updatedAt = Date(),
-                    stars = 1000
-                )
+    ): Disposable {
+        return Single.just(
+            RepoDetailModel(
+                title = "title",
+                repoName = "repoName",
+                ownerName = "ownerName",
+                ownerUrl = "",
+                followers = 1000,
+                following = 1000,
+                description = "description",
+                language = null,
+                updatedAt = Date(),
+                stars = 1000
             )
-            callback.onLoaded()
-        }, 1000)
+        ).delay(DELAY_TIME, TimeUnit.MILLISECONDS)
+            .observeOn(AndroidSchedulers.mainThread())
+            .doOnSubscribe {
+                callback.onLoading()
+            }.doOnTerminate {
+                callback.onLoaded()
+            }.subscribe({
+                callback.onSuccess(it)
+            }) {
+                callback.onError(it)
+            }
     }
 }
