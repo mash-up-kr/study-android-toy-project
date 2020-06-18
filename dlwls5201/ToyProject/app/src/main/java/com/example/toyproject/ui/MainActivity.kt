fe2b7c1bcf59ddc97a8915c09710ba56ad4e63d8
dlwls5201 <dlwls5201@naver.com>
2020-06-18 22:30:19 +0900

    MainActivity - DataBinding 적용

--- dlwls5201/ToyProject/app/src/main/java/com/example/toyproject/ui/MainActivity.kt
+++ dlwls5201/ToyProject/app/src/main/java/com/example/toyproject/ui/MainActivity.kt
@@ -2,13 +2,17 @@ package com.example.toyproject.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.example.toyproject.R
+import com.example.toyproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         initTitle()
         initMainFragment()
