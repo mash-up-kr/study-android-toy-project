40edd014eb5271b1a19b6bf5362245c40ce689ea
KangDahyun <dahyun1226@naver.com>
2020-07-16 14:43:58 +0900

    gradle ViewModel, LiveData 추가

--- dahyun1226/app/src/main/java/com/example/toyproject2020mvvm/ui/MainActivity.kt
+++ dahyun1226/app/src/main/java/com/example/toyproject2020mvvm/ui/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.toyproject2020mvvm.ui
 
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
